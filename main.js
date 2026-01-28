33d1041c0e0534ee7c4324f1057cbbf0599494b9
wchun055 <wchun055@uOttawa.ca>
2026-01-28 00:41:02 -0500

    Edit main.js for checkbox style profile tab.

--- main.js
+++ main.js
@@ -20,44 +20,6 @@ function openInfo(evt, tabName) {
 	evt.currentTarget.className += " active";
 
 }
-
-// generate a checkbox list from a list of products
-// it makes each product name as the label for the checkbos
-
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
-	
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
-    s2.innerHTML = "";
-		
-	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
-
-	// for each item in the array, create a checkbox element, each containing information such as:
-	// <input type="checkbox" name="product" value="Bread">
-	// <label for="Bread">Bread/label><br>
-		
-	for (i = 0; i < optionArray.length; i++) {
-			
-		var productName = optionArray[i];
-		// create the checkbox and add in HTML DOM
-		var checkbox = document.createElement("input");
-		checkbox.type = "checkbox";
-		checkbox.name = "product";
-		checkbox.value = productName;
-		s2.appendChild(checkbox);
-		
-		// create a label for the checkbox, and also add in HTML DOM
-		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
-		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
-	}
-}
 	
 // This function is called when the "Add selected items to cart" button in clicked
 // The purpose is to build the HTML to be displayed (a Paragraph) 
@@ -88,3 +50,177 @@ function selectedItems(){
 	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
 		
 }
+
+// Update the product tab HTML to have the correct products only.
+function populateProductList(prodList){
+    var displayProduct = document.getElementById("displayProduct");
+    displayProduct.innerHTML = "";
+		
+	for (const item of prodList){
+		var productName = item["name"];
+		var checkbox = document.createElement("input");
+
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+
+		displayProduct.appendChild(checkbox);
+		
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName));
+
+		displayProduct.appendChild(label);
+		
+		// create a breakline node and add in HTML DOM
+		displayProduct.appendChild(document.createElement("br"));    
+	}
+}
+
+// Creating a product list that adheres to the customer diet/profile.
+function updateProducts(profile){
+	updatedProducts = new Set([]); // Using a set to avoid duplicate additions.
+	toCheck = [];
+
+	var products = [
+	{
+		name: "lettuce",
+		vegetarian: true,
+		glutenFree: true,
+        organic: true,
+		price: 1.99
+	},
+	{
+		name: "whole wheat bread",
+		vegetarian: true,
+		glutenFree: false,
+        organic: false,
+		price: 2.35
+	},
+	{
+		name: "salmon",
+		vegetarian: false,
+		glutenFree: true,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "ground beef",
+		vegetarian: false,
+		glutenFree: true,
+        organic: false,
+		price: 15.49
+	},
+    {
+		name: "potato chips",
+		vegetarian: false,
+		glutenFree: false,
+        organic: false,
+		price: 4.30
+	},
+    {
+		name: "mango",
+		vegetarian: true,
+		glutenFree: true,
+        organic: true,
+		price: 5.00
+	},
+    {
+		name: "frozen pizza",
+		vegetarian: false,
+		glutenFree: false,
+        organic: false,
+		price: 4.99
+	},
+    {
+		name: "tomato",
+		vegetarian: true,
+		glutenFree: true,
+        organic: true,
+		price: 0.99
+	},
+    {
+		name: "strawberry",
+		vegetarian: true,
+		glutenFree: false,
+        organic: true,
+		price: 5.45
+	},
+    {
+		name: "celery",
+		vegetarian: false,
+		glutenFree: true,
+        organic: true,
+		price: 16.00
+	}
+];
+
+	for (let key in profile){
+		if (profile[key] == true){
+			toCheck.push(key);
+		}
+	}
+
+	for (let i = 0; i < products.length; i++){
+		for (let j = 0; j < toCheck.length; j++){
+			if (products[i][toCheck[j]] == true){
+				updatedProducts.add(products[i]);
+			}
+			else{
+				updatedProducts.delete(products[i]);
+				break;
+			}
+		}
+	}
+
+	populateProductList(updatedProducts) // Populate the HTML with the updated product list.
+}
+
+// Check which choices were made on profile.
+// Have a diet list and a accesibility list.
+function saveProfile(){
+    event.preventDefault();
+
+	const veggieCheck = document.getElementById("veggieCheck");
+	const glutenCheck = document.getElementById("glutenCheck");
+	const organicCheck = document.getElementById("organicCheck");
+	const fontCheck = document.getElementById("fontCheck");
+	const imageCheck = document.getElementById("imageCheck");
+
+	var diet = {
+		vegetarian : false,
+		glutenFree : false,
+		organic : false,
+	};
+
+	var accesibility = {
+		largeFont : false,
+		image : false
+	}
+
+	if (veggieCheck.checked){
+		diet["vegetarian"] = true;
+	}
+
+	if (glutenCheck.checked){
+		diet["glutenFree"] = true;
+	}
+
+	if (organicCheck.checked){
+		diet["organic"] = true;
+	}
+
+	if (fontCheck.checked){
+		accesibility["largeFont"] = true;
+	}
+
+	if (imageCheck.checked){
+		accesibility["image"] = true;
+	}
+
+	updateProducts(diet); // Call with our specific diet list to update the product list.
+	
+}
+
+//When the customer submits their profile, we begin updating their product list.
+document.getElementById("customerSubmit").addEventListener("click", saveProfile); 
